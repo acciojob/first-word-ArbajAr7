1f0c0d024ac498e4a054b185c927518bcf514251
Arbaj Ansari <83424578+ArbajAr7@users.noreply.github.com>
2024-05-09 01:39:02 +0545

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 function firstWord(s) {
   // your code here
+	let result ="";
+//   if(s == "")
+//   {
+//       return result;
+//   }
+  for(let i = 0; i<s.length; i++)
+  {
+      if(result.length == 0 && s[i]==" ")
+      {
+          i = i+1;
+      }
+      else if(result.length != 0 && s[i] == " ")
+      {
+          break;
+      }
+      result += s[i];
+  }
+  return result;
 }
 
 // Do not change the code below
